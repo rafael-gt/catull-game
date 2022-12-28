3fead238b91fefff49635d9483bdf5572232dd6b
Rafael <rafaelguenther100@gmail.com>
2022-12-28 01:35:23 +0100

    added transition between levels

--- Assets/Scripts/Gameplay/VictoryZone.cs
+++ Assets/Scripts/Gameplay/VictoryZone.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VictoryZone : MonoBehaviour
 {
@@ -8,5 +9,27 @@ public class VictoryZone : MonoBehaviour
     {
         //collider.transform.position = new Vector3(0, 0, 0);
         hearts.totalHearts = 9999;
+
+        StartCoroutine(DoAfterDelay(2)); 
+    }
+
+    IEnumerator DoAfterDelay(float time)
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(time);
+
+        int index =  SceneManager.GetActiveScene().buildIndex;
+
+        index = (index + 1) % 3;
+
+        SceneManager.LoadScene(index);
+        
+        hearts.totalHearts = 0;
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
 }
